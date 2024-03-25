a8f018beb9c2d5cefb9befb53853db211954d997
罗林 <exercisebank@163.com>
2024-03-25 22:06:05 +0800

    修复样式

--- vue.config.js
+++ vue.config.js
@@ -20,7 +20,6 @@ module.exports = {
     proxy: {
       "/": {
         // target: 'http://121.4.105.24', // 访问本地Linux服务器
-        // target: 'http://192.168.1.134', // 访问本地Linux服务器
         target: 'http://127.0.0.1:9008', // 本地url
         ws: true, // 是否启用websockets
         changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
